25dc37626d336b37299be8c112da99f4a09142d2
Kevin David <git@kevindavid.org>
2020-02-28 13:15:50 -0500

    Only use -H with whois on Linux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,8 +5,9 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias df='/bin/df -h'
 alias man='LC_ALL=C LANG=C man'
 alias ll='ls -al'
+
 if [ "$(uname -s)" = "Linux" ]
 then
     alias ls='ls --color=auto -CFX'
+    alias whois='whois -H'
 fi
-alias whois='whois -H'
