5ec9127767bbd62dd7c6f346866c33a37f9ed9ef
Kevin David <git@kevindavid.org>
2020-03-01 12:43:10 -0500

    Add python support

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,4 @@
 export GOPATH=$PROJECTS/go
-export PATH="$GOPATH/bin:$PATH"
+export PYTHONPATH=$HOME/Library/Python/3.7/lib/python/site-packages
+export PYTHON_BIN=$PYTHONPATH/../../../bin
+export PATH="$PYTHON_BIN:$GOPATH/bin:$PATH"
