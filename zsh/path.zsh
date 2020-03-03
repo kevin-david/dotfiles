3cf0e6130cf61b9980ea247672e7fe7b6dd7193c
Kevin David <git@kevindavid.org>
2020-03-03 08:50:00 -0500

    common path script, support ruby gems

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,7 @@
 export GOPATH=$PROJECTS/go
 export PYTHONPATH=$HOME/Library/Python/3.7/lib/python/site-packages
 export PYTHON_BIN=$PYTHONPATH/../../../bin
-export PATH="$PYTHON_BIN:$GOPATH/bin:$PATH"
+export GEM_HOME=$HOME/gems
+export GEM_BIN=$GEM_HOME/bin
+
+export PATH="$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$PATH"
