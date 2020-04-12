3f39136d64e2b662410c1aa1354c0bfe964e56d5
Kevin David <git@kevindavid.org>
2020-04-11 21:48:55 -0400

    Support linux pipfiles

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,4 +4,4 @@ export PYTHON_BIN=$PYTHONPATH/../../../bin
 export GEM_HOME=$HOME/gems
 export GEM_BIN=$GEM_HOME/bin
 
-export PATH="$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$PATH"
+export PATH="$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$HOME/.local/bin:$PATH"
