e55af6e0c3b5b12ef9174f75baf8b06f994a9a7d
Kevin David <git@kevindavid.org>
2021-02-22 09:19:42 -0500

    Fixing node on mac

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,5 +4,6 @@ export PYTHON_BIN=$PYTHONPATH/../../../bin
 export GEM_BIN=$HOME/gems/bin
 export RUBY_BIN=$HOME/.gem/ruby/2.6.0/bin
 export LOCAL_BIN=$HOME/.local/bin
+export NODE_BIN_PATH=/usr/local/opt/node@10/bin
 
-export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$PATH"
+export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$NODE_BIN_PATH:$PATH"
