e597f9e0955dfb10634f7ba9b649a3a7a4e0e0d5
Kevin David <git@kevindavid.org>
2026-04-30 12:21:38 -0400

    Minor path update, x-plat gh cmd

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,5 +5,6 @@ export GEM_BIN=$HOME/gems/bin
 export RUBY_BIN=$HOME/.gem/ruby/2.6.0/bin
 export LOCAL_BIN=$HOME/.local/bin
 export NODE_BIN_PATH=/usr/local/opt/node@10/bin
+export NPM_GLOBAL_BIN=$HOME/.npm-global/bin
 
-export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$NODE_BIN_PATH:$PATH"
+export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$NODE_BIN_PATH:$NPM_GLOBAL_BIN:$PATH"
