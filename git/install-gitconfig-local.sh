89bcda44a23de6c743a120100975cb9c01b1c0c6
Kevin David <git@kevindavid.org>
2026-04-30 14:35:10 -0400

    Install Atuin, create gitconfig installer script

---
+++ git/install-gitconfig-local.sh
@@ -0,0 +1,3 @@
+cp gitconfig.local.symlink.example gitconfig.local.symlink
+ln -s ~/.dotfiles/git/gitconfig.local.symlink ~/.gitconfig.local
+nano gitconfig.local.symlink
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,5 +6,6 @@ export RUBY_BIN=$HOME/.gem/ruby/2.6.0/bin
 export LOCAL_BIN=$HOME/.local/bin
 export NODE_BIN_PATH=/usr/local/opt/node@10/bin
 export NPM_GLOBAL_BIN=$HOME/.npm-global/bin
+export ATUIN_BIN=$HOME/.atuin/bin
 
-export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$NODE_BIN_PATH:$NPM_GLOBAL_BIN:$PATH"
+export PATH="$LOCAL_BIN:$RUBY_BIN:$GEM_BIN:$PYTHON_BIN:$GOPATH/bin:$NODE_BIN_PATH:$NPM_GLOBAL_BIN:$ATUIN_BIN:$PATH"
