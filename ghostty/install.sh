ced82c764e2d6d65f278f5909c13440d94307188
Kevin David <git@kevindavid.org>
2026-05-27 21:10:53 -0400

    Fix ghostty settings

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -8,7 +8,13 @@ set -e
 
 if [ "$(uname -s)" = "Darwin" ]; then
   echo "  Linking Ghostty config..."
+  config_source="$HOME/.dotfiles/ghostty/ghostty-mac.conf"
+
   mkdir -p "$HOME/.config/ghostty"
-  ln -sf "$HOME/.dotfiles/ghostty/ghostty-mac.conf" "$HOME/.config/ghostty/ghostty.conf"
+  ln -sf "$config_source" "$HOME/.config/ghostty/ghostty.conf"
+
+  mkdir -p "$HOME/Library/Application Support/com.mitchellh.ghostty"
+  ln -sf "$config_source" "$HOME/Library/Application Support/com.mitchellh.ghostty/config.ghostty"
+
   echo "  Done with Ghostty config"
 fi
