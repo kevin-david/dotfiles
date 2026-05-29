85fc3ac40788038ac3634d521ebee8d180427e83
Kevin David <git@kevindavid.org>
2026-05-29 11:46:45 -0400

    editor + diftool updates

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,8 @@
+# --wait blocks until the file is closed (git commit/rebase need this, or they
+# proceed before you've edited); --new opens a dedicated window instead of
+# tacking a tab onto an existing one.
 if [ "$(uname -s)" = "Darwin" ]; then
-    export EDITOR='zed'
+    export EDITOR='zed --wait --new'
 else
     export EDITOR='nano -w'
 fi
