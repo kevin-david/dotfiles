ded42aa8fa30f6ee5e7bc81ea3d742449b72f821
Kevin David <git@kevindavid.org>
2026-08-10 12:03:12 -0400

    chore(zsh): remove stale path entries

--- node/path.zsh
+++ node/path.zsh
@@ -7,6 +7,3 @@ else
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
-
-export NODE_BIN_PATH="$NVM_DIR/versions/node/$(nvm current)/bin"
-export PATH="$NODE_BIN_PATH:$PATH"
